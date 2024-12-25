bc16ca78e908a5631f00f9e15f2a11046311b6b0
Denis <94540315+Den1s0@users.noreply.github.com>
2024-12-25 14:37:00 +0300

    Create forms.py

---
+++ django/olimp/feads/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from .models import Olimp, Trier
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Olimp
+        fields = ('Title', 'Description','Site')
+        
+        
+class Students(forms.ModelForm):
+
+    class Meta:
+        model = Trier
+        fields = ('Name', 'LastName', 'Fomka','Olympics1','Olympics2','Olympics3',)
+        
+        
+        
+
+class SearchForms(forms.Form):
+    query = forms.CharField(label="Название олимпиады")
