f1989b70c35efab3d65e218b658a92a829178bae
Denis <94540315+Den1s0@users.noreply.github.com>
2024-12-25 14:36:27 +0300

    Update views.py

--- django/olimp/feads/views.py
+++ django/olimp/feads/views.py
@@ -1,10 +1,113 @@
-from re import S
-from django.shortcuts import render
-from .models import Post
-def olimp_list(request):
-    olimps = Post.objects.all()
-    s = []
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Olimp, Trier
+import random
+from .forms import PostForm, Students, SearchForms
+
+
+
+def olimp_detail(request,pk):
+    olimp = get_object_or_404(Olimp, pk=pk)
+    return render(request, 'feed/olimp_detail.html', {'olimp': olimp})
+
+
+
+def user_detail(request,pk):
+    trier = get_object_or_404(Trier, pk=pk)
+    olimp = ", ".join(list(map(str,list({trier.Olympics1,trier.Olympics2, trier.Olympics3,}))))
+#    for i in range(len(olimp)):
+#        olimp[i] = olimp[i][:-1]
+    
+    return render(request, 'feed/user_detail.html', {'trier': trier, 'olimp': olimp})
+
+
+def user_lists(request):
+    users = Trier.objects.all()
+    
+    s=[]
+    for trier in users:
+        
+        s.append(trier)
+
+    return render(request,'feed/user_lists.html' ,{'s': s})
+
+
+
+
+def olimp_lists(request):
+    olimps = Olimp.objects.all()
+    s=[]
     for olimp in olimps:
-        if 'inf' in olimp.tags:
-            s.append('olimp')
-    return render(request, 'feads/olimp_lists.html')
+        if 'inf' or 'mat' in olimp.tags:
+            s.append(olimp)
+            
+    
+    bic = {'table': [[ j*i for j in range(1,10)] for i in range(1,10) ] , 
+           "name": "Hekto", "role": "Admin", 
+           'bruh':[[ 'bruh ' for j in range(1,10)] for i in range(1,10) ],
+           'randommass': [[ random.randint(0,11) for j in range(1,10)] for i in range(1,10)],
+           'colors':['#FFC0CB',
+                     '#4B0082',
+                     '#FF0000',
+                     '#FF4500',
+                     '#F0E68C',
+                     '#3CB371',
+                     '#40E0D0',
+                     '#87CEEB',
+                     '#8B4513',
+                     '#2F4F4F'][random.randint(0,9)],
+                      's': s}
+    
+    return render(request,'feed/olimp_lists.html' ,bic)#{'s': s})
+
+
+def olimp_new(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('olimp_lists')
+    else:
+        return render(request, 'feed/olimp_add.html', {'form': form})
+
+
+
+
+
+
+def user_new(request):
+    
+    if request.method == "POST":
+        form = Students(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('user_lists')
+    else:
+        form = Students()
+    return render(request, 'feed/user_add.html', {'form': form})
+
+
+
+
+
+
+def olimp_search(request):
+    form = SearchForms()
+    query = None
+    result = []
+    if 'query' in request.GET:
+        form = SearchForms(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            result = Olimp.objects.filter(Title__contains=query)
+    return render(request, 'feed/olimp_search.html', {'form': form, 'query': query, 'result': result })
+
+
+
+
+
+
+
+
