6eeba6cdb80c994ada2870bd0827337329a4e994
Denis <94540315+Den1s0@users.noreply.github.com>
2024-12-25 14:36:08 +0300

    Update urls.py

--- django/olimp/feads/urls.py
+++ django/olimp/feads/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from . import views 
+    
 urlpatterns = [
-    path('', views.olimp_list, name='olimp_lists'),
+    path('', views.olimp_lists, name="olimp_lists"),
+    path('new/', views.olimp_new, name="olimp_new"),
+    path('olimp/<int:pk>/', views.olimp_detail, name="olimp_detail"),
+    path('user/<int:pk>/', views.user_detail, name="user_detail"),
+    path('newUser/', views.user_new, name="user_new"),
+    path('users/', views.user_lists, name="user_lists"),
+    path('search/', views.olimp_search, name="olimp_search"),
+    
 ]
